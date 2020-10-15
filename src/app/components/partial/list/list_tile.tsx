da561de2d40bb3ce83a5135377cb60cdbebd71dd
ItoYo16u <ito.yo16uh90616@gmail.com>
2020-10-15 20:40:11 +0900

    add list tile todo

---
+++ src/app/components/partial/list/list_tile.tsx
@@ -0,0 +1,22 @@
+/** @jsx jsx */
+import * as React from "react";
+import {css,jsx} from "@emotion/core"
+
+interface IProps {
+  // TODO: インターフェースを定義する
+}
+/**
+ * NOTE: checkboxlisttile はtitle,subtitle,checkboxを表示する.
+ * NOTE: checkboxlisttile をタップするとcallback関数が呼ばれた後にcheckboxのチェックが切り替わる.
+ * NOTE: checkboxlisttile はcheckがついたとき(onChecked)とcheckが外されたとき(onUnchecked)でことなるコールバック関数を呼ぶ.
+ */
+export const CheckBoxListTile: React.FC<IProps> = ({ }) => {
+  
+  //TODO: 状態を定義する
+  
+  //TODO: レイアウトを定義する
+  return (
+    <div> 
+    </div>
+  )
+}
